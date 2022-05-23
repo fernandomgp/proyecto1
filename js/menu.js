ff5d2f61ea7f6d55dc27c10ef2a6e8785d72cee1
Fernando <fernandomgp17@gmail.com>
2022-05-23 15:47:27 -0500

    Agregado de javascript LightBox y modificacion del closeLight

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,8 @@
 const hamburguer =  document.querySelector('.hamburguer');
 const menu =  document.querySelector('.menu-navegacion');
 
-console.log(menu);
-console.log(hamburguer);
+// console.log(menu);
+// console.log(hamburguer);
 
 hamburguer.addEventListener('click',()=>{
     menu.classList.toggle('spread')
